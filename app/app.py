710b9182b3980188cb3ef4321ecd126fce7dd5dd
Stefano Passador <stefano.passador.bip@s>
2020-10-18 15:02:44 +0200

    Add comments on app.py

--- app/app.py
+++ app/app.py
@@ -9,19 +9,19 @@ db_pass = 'secret'
 db_host = 'db'
 db_port = '5432'
 
-#postgres://username:secret@db:5432/database
+# Connecto to the database
 db_string = 'postgres://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
 db = create_engine(db_string)
 
 def add_new_row(n):
-  db.execute("INSERT INTO numbers ("+\
-            "number,"+\
-            "timestamp) "+\
-        "VALUES ("+\
-            str(n) + "," + \
-            str(int(round(time.time() * 1000))) + ");")
+    # Insert a new number into the 'numbers' table.
+    db.execute("INSERT INTO numbers (number,timestamp) "+\
+        "VALUES ("+\ 
+        str(n) + "," + \
+        str(int(round(time.time() * 1000))) + ");")
 
 def get_last_row():
+    # Retrieve the last number inserted inside the 'numbers'
     query = "" + \
             "SELECT number " + \
             "FROM numbers " + \
@@ -36,6 +36,6 @@ if __name__ == '__main__':
     print('Application started')
     
     while True:
-        time.sleep(3)
+        time.sleep(5)
         add_new_row(random.randint(1,100000))
         print('The last value insterted is: {}'.format(get_last_row()))
