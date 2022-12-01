e88659d6175f58eac900a7f9a9452d0f66ef7df3
Stefano Passador <stefanopassador@users.noreply.github.com>
2022-12-01 08:49:38 +0000

    Upgrade to new SQLVersion

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ db_host = 'db'
 db_port = '5432'
 
 # Connecto to the database
-db_string = 'postgres://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
+db_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
 db = create_engine(db_string)
 
 def add_new_row(n):
